fbacf51cefdd0a8f6e418421dfb0fedf93c98265
reemsaleh01 <91529300+reemsaleh01@users.noreply.github.com>
2021-11-21 17:10:42 +0300

    Update UI (#1)

---
+++ MemoryGame/ImagesCards.swift
@@ -0,0 +1,17 @@
+//
+//  ImagesCards.swift
+//  MemoryGame
+//
+//  Created by Reem Saleh on 16/04/1443 AH.
+//
+
+import UIKit
+
+class ImagesCards: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
