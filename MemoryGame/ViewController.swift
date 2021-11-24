ce852edbb5c65f0b7567171bbbe58d257eae6b71
nujudalharbi <91524880+nujudalharbi@users.noreply.github.com>
2021-11-24 09:48:31 +0300

    Work code (#3)
    * write some code
    
    * more design
    
    * update cell of collection view
    
    * delete image in collection game
    
    * Update ViewController.swift
    
    Co-authored-by: AfnanTheeb <91525168+AfnanTheeb@users.noreply.github.com>

--- MemoryGame/ViewController.swift
+++ MemoryGame/ViewController.swift
@@ -8,21 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController , UICollectionViewDelegate , UICollectionViewDataSource  {
+        
+    var imgeCardArr = [
+        UIImage(named: "moon1"),
+        UIImage(named: "moon1") ,
+        UIImage(named: "bee1") ,
+        UIImage(named: "bee1") ,
+        UIImage(named: "green1") ,
+        UIImage(named: "green1") ,
+        UIImage(named: "brid1") ,
+        UIImage(named: "bird1") ,
+        UIImage(named: "star1") ,
+        UIImage(named: "star1") ,
+        UIImage(named: "cake1") ,
+        UIImage(named: "cake1") ,
+        UIImage(named: "parrot1") ,
+        UIImage(named: "parrot1") ,
+        UIImage(named: "sweat1"),
+        UIImage(named: "sweat1")]
     
-//    var cardImges = [Cards]()
-    let imgeCardArr = [UIImage(named: "moon1")]
     
     //     timer ----------------
     @IBOutlet weak var timerLable: UILabel!
-//    var timer : Timer?
-//    var milliSeconds : Float = 30 * 1000
-//
-//
-
     
-
+    
+    
+    
     @IBOutlet weak var collectionView: UICollectionView!
-//  -------------   func delegate
+    //  -------------   func delegate
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return imgeCardArr.count
@@ -30,20 +43,44 @@ class ViewController: UIViewController , UICollectionViewDelegate , UICollection
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cardID", for: indexPath) as! ImagesCards
-           cell.images.image = imgeCardArr[0]
+        cell.images.image = imgeCardArr[indexPath.row ]
         return cell
     }
+    //    ---------------
     
     
+    
+    var lastSelectedIndex : Int?
+    
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let cellObject = collectionView.cellForItem(at: indexPath) as! ImagesCards
+        print("selected image: \(cellObject.images.image)")
+        print("prev image: \(imgeCardArr[indexPath.row])")
+        if lastSelectedIndex != nil  {
+            if (imgeCardArr[lastSelectedIndex!] == cellObject.images.image && lastSelectedIndex != indexPath.row) {
+                imgeCardArr.remove(at: lastSelectedIndex!)
+                imgeCardArr.remove(at: indexPath.row)
+                print("Matched")
+                lastSelectedIndex = nil
+            }
+        } else {
+            print("Keep going")
+            lastSelectedIndex = indexPath.row
+        }
+        collectionView.reloadData()
+        print("Last index: \(lastSelectedIndex), current Index: \(indexPath.row)")
 
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-      
         collectionView.delegate = self
         collectionView.dataSource = self
         
         collectionView.register(UINib(nibName: "ImagesCards", bundle: nil), forCellWithReuseIdentifier: "cardID")
-        //        create timer
+        
+            //        create timer
 //        timer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(timerElapsed), userInfo: nil, repeats: true )
 //
 //        RunLoop.main.add(timer! , forMode: .common)
@@ -54,6 +91,26 @@ class ViewController: UIViewController , UICollectionViewDelegate , UICollection
     }
     
     
+
+    //    func createTimer (){
+    //        let timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(time), userInfo: nil, repeats: false)
+    //    }
+    //
+    //
+    //    @objc func time (){
+    //
+    //        view.backgroundColor = .systemRed
+    //    }
+    //
+    
+    
+    
+    
+    
+    
+}
+
+
     
 //    timer func -----------------
 //    @objc func timerElapsed(){
@@ -75,8 +132,7 @@ class ViewController: UIViewController , UICollectionViewDelegate , UICollection
         
         
         
-        
-    }
+   
     
     
     
---
+++ MemoryGame/homeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  homeViewController.swift
+//  MemoryGame
+//
+//  Created by نجود  on 18/04/1443 AH.
+//
+
+import UIKit
+
+class homeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
